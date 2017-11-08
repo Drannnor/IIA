c57eb8d2521323717d2cd87ca752562d048a577a
Drannnor <33358630+Drannnor@users.noreply.github.com>
2017-11-08 16:43:35 +0000

    Add files via upload

--- p2.py
+++ p2.py
@@ -6,12 +6,28 @@ def outro_player(p) :
 def F1(state,player):
             return len(state[2][1][player]) - len(state[2][1][outro_player(player)])
 
-def F2(state,player)
-sum = 0
-            for (x,y) in state[2][1][player]
-                        sum = sum + y - 2 
+def F2(state,player):
+
+           distsPl = [pc[1] for pc in state[2][1][player]]
+           distsAd = [pc[1] for pc in state[2][1][outro_player(player)]]
+           print (distsAd)
+           print(distsPl)
+           if(player == 'brancas'):
+                        f2Pl =  sum(map(lambda x: 2**(x - 2),distsPl)) / len( state[2][1][player])
+                        f2Ad = sum(map(lambda x: 2**(7 - x),distsAd)) / len( state[2][1][player])
+           else:
+                        f2Pl = sum(map(lambda x: 2**(7 - x),distsPl)) / len( state[2][1][player])
+                        f2Ad =  sum(map(lambda x: 2**(x - 2),distsAd)) / len( state[2][1][player])
+
+
+           print (f2Pl)
+           print (f2Ad)
+           return f2Pl - f2Ad
+
+
+
 st = input()
 if st.isdigit():
-            print (F1(teste(int(st)),input()))
+            print (F2(teste(int(st)),input()))
 else:
-           print (F1(eval(st),input()))  
+           print (F2(eval(st),input()))  
